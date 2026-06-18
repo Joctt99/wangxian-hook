40072ea49300b2c763465c489ca5d288e4b8d62c
Joctt99 <130507690+Joctt99@users.noreply.github.com>
2026-06-19 02:23:26 +0800

    Update stub_signature/StubSignatureCheck.m

---
+++ stub_signature/StubSignatureCheck.m
@@ -0,0 +1,24 @@
+/**
+ * Stub lnSignature.dylib - provides SignatureCheck class
+ * All methods are empty stubs to satisfy symbol resolution.
+ */
+#import <Foundation/Foundation.h>
+
+@interface SignatureCheck : NSObject
++ (void)load;
++ (void)JudgeApp;
++ (void)GetApp;
++ (void)PostApp;
++ (void)showTipViewEND:(id)arg;
++ (void)exitApplication;
+@property (nonatomic, retain) id nettimes;
+@end
+
+@implementation SignatureCheck
++ (void)load {}
++ (void)JudgeApp {}
++ (void)GetApp {}
++ (void)PostApp {}
++ (void)showTipViewEND:(id)arg {}
++ (void)exitApplication {}
+@end
