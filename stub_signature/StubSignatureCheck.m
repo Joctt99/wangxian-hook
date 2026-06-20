d7027fedb2a289668a58d30a843509305e8a133e
Joctt99 <130507690+Joctt99@users.noreply.github.com>
2026-06-20 19:03:34 +0800

    Update stub_signature/StubSignatureCheck.m

--- stub_signature/StubSignatureCheck.m
+++ stub_signature/StubSignatureCheck.m
@@ -1,4 +1,102 @@
 /**
+ * Stub lnSignature.dylib v3 - SignatureCheck with real API calls
+ * +load: initial attempt (may fail if WangXianHook not loaded yet)
+ * JudgeApp: retry during login (WangXianHook hooks should be active)
+ */
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+static NSDictionary *g_verifyResult = nil;
+static BOOL g_verifyPassed = NO;
+
+@interface SignatureCheck : NSObject
++ (void)load;
++ (void)JudgeApp;
++ (void)GetApp;
++ (void)PostApp;
++ (void)showTipViewEND:(id)arg;
++ (void)exitApplication;
+@property (nonatomic, retain) id nettimes;
+@end
+
+@implementation SignatureCheck
+
++ (void)doVerify:(NSString *)caller {
+    NSLog(@"[StubSig] doVerify from %@ - current passed=%d", caller, g_verifyPassed);
+    
+    // Hardcoded bundle ID (NSBundles mainBundle may be nil in +load)
+    NSString *bundleId = @"com.sqage.wangxianapp";
+    NSString *udid = [[[UIDevice currentDevice] identifierForVendor] UUIDString] ?: @"UNKNOWN-UDID";
+    
+    NSString *urlStr = [NSString stringWithFormat:
+        @"http://ln_sign_cert.9iy.com/cert/judgeAppInfoApi?APPID=%@&UDID=%@",
+        bundleId, udid];
+    
+    NSLog(@"[StubSig] API URL: %@", urlStr);
+    
+    NSURL *url = [NSURL URLWithString:urlStr];
+    if (!url) {
+        NSLog(@"[StubSig] ERROR: invalid URL");
+        return;
+    }
+    
+    NSURLRequest *req = [NSURLRequest requestWithURL:url
+                                          cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
+                                      timeoutInterval:15];
+    
+    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+    __block NSData *respData = nil;
+    __block NSError *respErr = nil;
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession]
+        dataTaskWithRequest:req
+        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            respData = data;
+            respErr = error;
+            dispatch_semaphore_signal(sem);
+        }];
+    [task resume];
+    dispatch_semaphore_wait(sem, dispatch_time(DISPATCH_TIME_NOW, 15 * NSEC_PER_SEC));
+    
+    if (respData) {
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:respData options:0 error:nil];
+        NSString *rawStr = [[NSString alloc] initWithData:respData encoding:NSUTF8StringEncoding];
+        NSLog(@"[StubSig] API raw response: %@", rawStr);
+        if (json) {
+            g_verifyResult = json;
+            NSString *ispass = json[@"ispass"];
+            if (!ispass) ispass = json[@"result"][@"ispass"];
+            NSLog(@"[StubSig] ispass=%@", ispass);
+            if ([ispass isEqualToString:@"YES"]) {
+                g_verifyPassed = YES;
+                NSLog(@"[StubSig] PASSED!");
+            }
+        }
+    } else {
+        NSLog(@"[StubSig] API error: %@", respErr);
+    }
+}
+
++ (void)load {
+    NSLog(@"[StubSig] +load - attempting initial verification");
+    [self doVerify:@"+load"];
+    NSLog(@"[StubSig] +load done, passed=%d", g_verifyPassed);
+}
+
++ (void)JudgeApp {
+    NSLog(@"[StubSig] JudgeApp called, passed=%d", g_verifyPassed);
+    // Always re-verify - WangXianHook should be loaded by now
+    [self doVerify:@"JudgeApp"];
+    NSLog(@"[StubSig] JudgeApp done, passed=%d", g_verifyPassed);
+}
+
++ (void)GetApp { NSLog(@"[StubSig] GetApp"); }
++ (void)PostApp { NSLog(@"[StubSig] PostApp"); }
++ (void)showTipViewEND:(id)arg { NSLog(@"[StubSig] showTipViewEND: %@", arg); }
++ (void)exitApplication { NSLog(@"[StubSig] exitApplication BLOCKED"); }
+
+@end
+/**
  * Stub lnSignature.dylib v2 - SignatureCheck that actually works
  * Calls the real API and stores the result for the game to use
  */
