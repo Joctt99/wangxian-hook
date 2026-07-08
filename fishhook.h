79be31dda3ca3281496c7fd854b2e17a5fe446d6
Joctt99 <130507690+Joctt99@users.noreply.github.com>
2026-07-09 03:52:49 +0800

    v4.0: fishhook library header

---
+++ fishhook.h
@@ -0,0 +1,34 @@
+#ifndef fishhook_h
+#define fishhook_h
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct rebinding {
+  const char *name;
+  void *replacement;
+  void **replaced;
+};
+
+struct rebindings_entry {
+  struct rebinding *rebindings;
+  size_t rebindings_nel;
+  struct rebindings_entry *next;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
+
+int rebind_symbols_image(void *header,
+                         intptr_t slide,
+                         struct rebinding rebindings[],
+                         size_t rebindings_nel);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
